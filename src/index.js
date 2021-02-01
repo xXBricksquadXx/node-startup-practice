18278f809492dafb4a93a313ffac3f09caba1882
Colby Cardell <Colby88cardell@gmail.com>
2021-02-01 18:22:31 -0500

    :recycle: Destructure score from the student object

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
-import { studentData } from "./lib";
-
-function getLastName(name) {
-  return name.split(" ")[1];
-}
+// have to destructure the named export from lib
+import { getLastName, studentData } from "./lib";
 
 // get a list of all student last names and ids
 // only those that scored below 70
 const lowScores = studentData
-  .filter((student) => student.score <= 70)
+  .filter(({ score }) => score <= 70)
   .map((student) => ({
     lastName: getLastName(student.name),
     id: student.id,
