c34642fb6c884297b8890374b4859b8413dc64d1
Colby Cardell <Colby88cardell@gmail.com>
2021-02-01 18:13:33 -0500

    :sparkles: Logged split- filter to get student lname and score below 70

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { studentData } from "./lib";
 
+function getLastName(name) {
+  return name.split(" ")[1];
+}
+
 // get a list of all student last names and ids
 // only those that scored below 70
 const lowScores = studentData
   .filter((student) => student.score <= 70)
   .map((student) => ({
-    lastName: student.name,
+    lastName: getLastName(student.name),
     id: student.id,
   }));
 
