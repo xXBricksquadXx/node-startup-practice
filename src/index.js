5e8bf3c5f3c47ef8d6d2bcfa8fcf61a1c65fd201
Colby Cardell <Colby88cardell@gmail.com>
2021-02-02 13:12:45 -0500

    Log 'lowScore'

--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
-import { getBirthYear, senatorsData } from "./lib";
+import { studentData } from "./lib";
 
-// TODO we need to make a list of all female senators to call them and invite to a special event (we need phone and name)
+console.log(studentData);
 
-// TODO Get a list of jut the name field from the senators if they were born since 1960
-const femaleSenatorNames = senatorsData
-  .filter(({ person }) => person.gender === "female")
-  .map(({ person }) => ({ name: person.name }));
+const lowScores = studentData.filter((student) => student.score < 70);
 
-// console.log(femaleSenatorNames);
-
-const youngSenators = senatorsData
-  .filter(({ person }) => getBirthYear(person.birthday) > 1960)
-  .map(({ person }) => ({ name: person.name }));
-
-console.log(youngSenators);
+console.log(lowScores);
