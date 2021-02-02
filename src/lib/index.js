62ed9c8ea54693ed301f7e5588f62a79569c7602
Colby Cardell <Colby88cardell@gmail.com>
2021-02-01 20:07:22 -0500

    Add 'getBirthYear' to the library

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,20 +1,3 @@
 export { default as senatorsData } from "./senators-data";
-export { default as studentData } from "./student-data";
-export const assignLetterGrade = (score) => {
-  if (score > 90) {
-    return "A";
-  }
-  if (score > 80) {
-    return "B";
-  }
-  if (score > 70) {
-    return "C";
-  }
-  if (score > 60) {
-    return "D";
-  }
-  return "F";
-};
 
-export const curvedScore = (score, curve) => score + curve;
-export const getLastName = (name) => name.split(" ")[1];
+export const getBirthYear = (bday) => bday.slice(0, 4);
