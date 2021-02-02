a477fea75f2da3018c5fc6b524089a09cf6f0868
Colby Cardell <Colby88cardell@gmail.com>
2021-02-02 13:42:22 -0500

    Move 'getLastName' into 'Lib'

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,1 +1,5 @@
+// Re-exporting out whatever 'data' exports out as default.
+// Assigning it the name 'studentData'
 export { default as studentData } from "./student-data";
+
+export const getLastName = (name) => name.split(" ")[1];
