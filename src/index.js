807d0111479c1c20e65a3fcf7e9555e0475e46e6
Colby Cardell <Colby88cardell@gmail.com>
2021-02-02 15:13:09 -0500

    Add example of dem senators nickname

--- src/index.js
+++ src/index.js
@@ -1,22 +1,24 @@
-import { assignLetterGrade, curveScore, getLastName, studentData } from "./lib";
+// Have to destructure the named export from lib
+import { senatorsData } from "./lib";
 
-// TODO: Get a list of just the 'name' field from the senators if they were born since 1960
+// TODO create new array {fullNickname: 'michael' 'mike' crapo' link' twitterid} - "Democrats"
 
-const lowScores = studentData
-  .filter(({ score }) => score < 70)
-  .map(({ name, id }) => ({
-    lastName: getLastName(name),
-    id,
-  }));
+const demSenators = senatorsData
+  .filter(({ party }) => party === "Democrat")
+  .map(
+    ({
+      person: {
+        firstname: firstName,
+        nickname: nickName,
+        lastname: lastName,
+        link,
+        twitterid: twitter,
+      },
+    }) => ({
+      fullNickName: `${firstName} ${nickName} ${lastName}`,
+      link,
+      twitter,
+    })
+  );
 
-const scoresWithLetterGrades = studentData.map((student) => ({
-  grade: assignLetterGrade(student.score),
-  ...student,
-}));
-
-const curvedScores = studentData.map((student) => ({
-  curvedScore: curveScore(student.score, 10),
-  ...student,
-}));
-
-console.log(curvedScores);
+console.log(demSenators);
