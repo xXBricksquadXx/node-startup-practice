4227ebd190cfbc6f2500fbc4c1801e4ec19e793f
Colby Cardell <Colby88cardell@gmail.com>
2021-02-01 20:08:13 -0500

    Get Young Sens && Female Sens

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
-import { senatorsData } from "./lib";
+import { getBirthYear, senatorsData } from "./lib";
 
 // TODO we need to make a list of all female senators to call them and invite to a special event (we need phone and name)
 
 // TODO Get a list of jut the name field from the senators if they were born since 1960
-const youngSenators = senatorsData.map(({ person }) => ({ name: person.name }));
+const femaleSenatorNames = senatorsData
+  .filter(({ person }) => person.gender === "female")
+  .map(({ person }) => ({ name: person.name }));
+
+// console.log(femaleSenatorNames);
+
+const youngSenators = senatorsData
+  .filter(({ person }) => getBirthYear(person.birthday) > 1960)
+  .map(({ person }) => ({ name: person.name }));
 
 console.log(youngSenators);
