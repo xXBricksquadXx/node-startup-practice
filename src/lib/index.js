fdebbcdbb4594cdab9de9bb8923115128ca04cc7
Colby Cardell <Colby88cardell@gmail.com>
2021-02-01 19:35:09 -0500

    added if to return letter grades

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,3 +1,20 @@
-export { default as studentData } from "./data";
+export { default as senatorsData } from "./senators-data";
+export { default as studentData } from "./student-data";
+export const assignLetterGrade = (score) => {
+  if (score > 90) {
+    return "A";
+  }
+  if (score > 80) {
+    return "B";
+  }
+  if (score > 70) {
+    return "C";
+  }
+  if (score > 60) {
+    return "D";
+  }
+  return "F";
+};
 
+export const curvedScore = (score, curve) => score + curve;
 export const getLastName = (name) => name.split(" ")[1];
