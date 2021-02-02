1ef63f755bcbe8280065014a2f0092f78731a343
Colby Cardell <Colby88cardell@gmail.com>
2021-02-02 13:31:11 -0500

    Get the student last name in filtered data

--- src/index.js
+++ src/index.js
@@ -7,19 +7,14 @@ function getLastName(name) {
    * 2. Return [1] (index 1)
    */
 
-  return name.split(" ");
+  return name.split(" ")[1];
 }
 
-console.log(getLastName("Harry Wolff"));
-
-// console.log(studentData);
-// Get a list of all students last names and ids - only those that scored below 70
-// filter-map chain
 const lowScores = studentData
   .filter((student) => student.score < 70)
   .map((student) => ({
-    lastName: student.name,
+    lastName: getLastName(student.name),
     id: student.id,
   }));
 
-// console.log(lowScores);
+console.log(lowScores);
