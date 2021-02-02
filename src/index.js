e2d6d649ec6fc587010af1af788989ffc956e7f5
Colby Cardell <Colby88cardell@gmail.com>
2021-02-01 19:35:37 -0500

    added curved score

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 // have to destructure the named export from lib
-import { getLastName, studentData } from "./lib";
+import {
+  assignLetterGrade,
+  curvedScore,
+  getLastName,
+  studentData,
+} from "./lib";
 
 // get a list of all student last names and ids
 // only those that scored below 70
@@ -10,4 +15,17 @@ const lowScores = studentData
     id,
   }));
 
-console.log(lowScores);
+// TODO: map over our student and add a letter grade to the data
+const scoresWithLetterGrades = studentData.map((student) => {
+  const ret = { letterGrade: assignLetterGrade(student.score), ...student };
+
+  return ret;
+});
+console.log(scoresWithLetterGrades);
+
+const curvedScores = studentData.map((student) => ({
+  curvedScore: curvedScore(student.score, 10),
+  ...student,
+}));
+
+console.log(curvedScores);
