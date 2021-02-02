5d1face0f79b7d1189fb35af56277390799a68b7
Colby Cardell <Colby88cardell@gmail.com>
2021-02-02 13:38:17 -0500

    Destructure score from student object

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import { studentData } from "./lib";
-
-function getLastName(name) {
-  /**
-   * Assume that there is a space and it's 'first last'
-   * 1. TODO: Turn this into an array ['first', 'last']
-   * 2. Return [1] (index 1)
-   */
-
-  return name.split(" ")[1];
-}
+import { getLastName, studentData } from "./lib";
 
 const lowScores = studentData
-  .filter((student) => student.score < 70)
+  .filter(({ score }) => score < 70)
   .map((student) => ({
     lastName: getLastName(student.name),
     id: student.id,
