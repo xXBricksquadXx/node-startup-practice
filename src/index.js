fb4f2c9d5e81914287fb01323d35a4270eb4c1ed
Colby Cardell <Colby88cardell@gmail.com>
2021-02-04 12:33:46 -0500

    Practice pulling 'studentData' returning with a letter grade

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ import { assignLetterGrade, studentData } from "./lib";
 
 const studentDataWithGrades = studentData.map((student) => ({
   ...student,
-  grade: assignLetterGrade(student.score),
+  letterGrade: assignLetterGrade(student.score),
 }));
 
-const aStudents = studentDataWithGrades.filter(({ grade }) => grade === "A");
-
-const aStudentsAvg = (
-  aStudents.reduce((total, { score }) => total + score, 0) / aStudents.length
-).toFixed(2);
-
-console.log(aStudentsAvg);
+console.log(studentDataWithGrades);
